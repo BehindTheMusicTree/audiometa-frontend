bbaadd2f23400875fa7cf9c1a930c866a1b7becf
mignot <garcia.andreas.1991@gmail.com>
2026-04-05 14:08:13 +0200

    feat: enhance localization support by adding new languages and updating layout direction for RTL; improve language switcher functionality

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -1,7 +1,7 @@
 import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
-  locales: ["en", "fr", "zh"],
+  locales: ["en", "fr", "zh", "pt", "ja", "ar", "de", "es"],
   defaultLocale: "en",
   localePrefix: "as-needed",
 });
